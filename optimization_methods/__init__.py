9021c64d190024c88f70f340caa70059e72931e1
Alexander Sidorenko <xarendel@gmail.com>
2025-03-28 02:10:32 +0300

    Project structure has been changed: GradientHelper class has been removed, and other minor changes

---
+++ optimization_methods/__init__.py
@@ -0,0 +1,5 @@
+# optimization_methods/__init__.py
+
+from .gradient_descent import GradientDescent
+
+__all__ = ["GradientDescent"]
---
+++ utils/__init__.py
@@ -0,0 +1,5 @@
+# utils/__init__.py
+
+from .log_emitter import LogEmitter
+
+__all__ = ["LogEmitter"]
